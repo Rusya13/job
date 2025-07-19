c4443d9de55c5b580a6dc3961d1d2a280f77528b
slqnt <94934670+slqntdevss@users.noreply.github.com>
2025-07-18 22:47:08 -0400

    improve tabs

--- public/assets/js/tabs.js
+++ public/assets/js/tabs.js
@@ -9,9 +9,31 @@ function addTab() {
 
   const newTab = document.createElement("div");
   newTab.classList.add("tab");
-  newTab.innerHTML = "<span>New tab</span>";
   newTab.id = tabId;
+  const title = document.createElement("span");
+  title.innerText = "New Tab";
+  newTab.appendChild(title);
   tabContainer.insertBefore(newTab, addTabButton);
+
+  const closeButton = document.createElement("button");
+  closeButton.classList.add("close");
+  closeButton.addEventListener("click", () => closeTab(tabId));
+  closeButton.innerHTML = `<svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-x-icon lucide-x"
+          >
+            <path d="M18 6 6 18" />
+            <path d="m6 6 12 12" />
+          </svg>`;
+  newTab.appendChild(closeButton);
 
   const page = document.createElement("iframe");
   page.src = "home.html";
@@ -25,8 +47,7 @@ function addTab() {
   newTab.addEventListener("click", () => activateTab(tabId));
   page.addEventListener("load", () => {
     //im very laazy so its jjust gonna stay like this
-    console.log("wghat the flip");
-    newTab.innerHTML = `<span>${page.contentDocument.title}</span>`;
+    newTab.querySelector("span").innerText = page.contentDocument.title;
   });
 }
 function activateTab(tabId) {
@@ -45,10 +66,11 @@ function activateTab(tabId) {
 }
 function closeTab(tabId) {
   console.log(activeTabs);
+  console.log("aeahefh");
   if (activeTabs.length <= 1) return;
   const currentTab = activeTabs.findIndex((tab) => tab.id === tabId);
   if (currentTab == null) return;
-  activeTabs.splice(currentTab);
+  activeTabs.splice(currentTab, 1);
   document.getElementById(tabId).remove();
   //retarded way t do this but idc
   document.getElementById(tabId + "-f").remove();
